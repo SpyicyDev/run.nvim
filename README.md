d55f4ba361802ba2cf4e65f9761e93cb975a06d9
Mack Haymond <mack.haymond@icloud.com>
2024-01-09 21:07:26 -0500

    got rid of deep_copy function, updated README

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 ## TODO
 
 1. Optional filetype in TOML spec, limits that entry to only appear if in the right filetype, goes right to filetype default if no entries and shows no menu
-2. Comment code
-4. maybe .run.lua? some other file?
--- lua/run/utils.lua
+++ lua/run/utils.lua
@@ -2,19 +2,6 @@ local M = {}
 
 local config = require("run.config")
 
-M.deep_copy = function(obj, seen)
-    -- Handle non-tables and previously-seen tables.
-    if type(obj) ~= 'table' then return obj end
-    if seen and seen[obj] then return seen[obj] end
-
-    -- New table; mark it as seen and copy recursively.
-    local s = seen or {}
-    local res = {}
-    s[obj] = res
-    for k, v in pairs(obj) do res[M.deep_copy(k, s)] = M.deep_copy(v, s) end
-    return setmetatable(res, getmetatable(obj))
-end
-
 M.fmt_cmd = function(cmd)
     if string.find(cmd, "%%f") then
         cmd = string.gsub(cmd, "%%f", vim.api.nvim_buf_get_name(0))
